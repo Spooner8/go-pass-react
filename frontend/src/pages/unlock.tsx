734878bae5b04399f8c07e2581cbf5d45c0a1de0
Spooner <spooner@mmcs.ch>
2024-12-24 13:09:34 +0100

    remove unnessesari css and add it as bootstrap class directly

--- frontend/src/pages/unlock.tsx
+++ frontend/src/pages/unlock.tsx
@@ -4,13 +4,11 @@ import { UnlockForm } from "../components/unlockForm";
 export function Unlock() {
 
   return (
-    <div id="unlock" className="container">
+    <div id="unlock" className="container d-flex justify-content-center align-items-center flex-column">
       <img id="unlock-logo"src={logo} alt="Logo" className="my-5"/>
-      <div className="row justify-content-center mx-auto w-100">
         <div className="col-sm-12 col-md-6 col-lg-6">
           <UnlockForm />
         </div>
-      </div>
     </div>
   );
 }
